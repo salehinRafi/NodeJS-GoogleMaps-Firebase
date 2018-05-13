622e0bea282b4c0dcf8a8c881d9a2e64c02f723c
salehinRafi <salehinrafi35@gmail.com>
2018-05-13 16:28:06 +0800

    Connection to Firebase

--- controllers/loc.controller.js
+++ controllers/loc.controller.js
@@ -1,6 +1,32 @@
+var firebase = require("firebase");
+var serviceAccount = require("../config/firebase.json");
+firebase.initializeApp({
+    credential: serviceAccount,
+    databaseURL: "https://node-client-e6a27.firebaseio.com"
+});
+
 module.exports = {
     findAll(request, response) {
-        var locations2 = [{
+
+        var ref = firebase.database().ref('/');
+        var locationRef = ref.child("maps");
+
+        var locations = locationRef.on("value", (snapshot) => {
+            console.log(snapshot.key);
+            exports.getItems = () => {
+                /* Get The required value and export it so as to be accessed from client*/
+                var info = {};
+                var key = snapshot.key;
+                info[key] = [];
+                info[key].push(snapshot.val());
+
+                return info;
+            }
+        });
+
+        /* Hard code location */
+        /*
+        var locations = [{
                 "name": "Mimos",
                 "lat": 3.047789,
                 "long": 101.696253,
@@ -11,12 +37,10 @@ module.exports = {
                 "long": 101.686895,
             }
         ];
-        var locations = [
-            ['Mimos', 3.047789, 101.696253, 1],
-            ['Bernama', 3.047723, 101.686895, 2]
-        ];
+        */
+
         // return all the jobs to the server with 200 status
-        return response.status(200).json(locations2);
+        return response.status(200).json(locations);
 
     }
 };
